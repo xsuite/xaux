6785b78ee4bbf070203a4cadd75648297f60c036
kparasch <konstantinos.paraschou@cern.ch>
2024-02-28 18:00:28 +0100

    blacked test helpers

--- xaux/test_helpers.py
+++ xaux/test_helpers.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import shutil
 from . import ProtectFile
 
+ProtectFile._debug = True
+
+
 def rewrite(pf, with_copy=False):
     data = json.load(pf)
     time.sleep(0.2)
@@ -19,15 +22,18 @@ def rewrite(pf, with_copy=False):
         shutil.copyfile(cfname, pf.name)
         Path.unlink(Path(cfname))
 
+
 def change_file_protected(fname, with_copy=False, max_lock_time=None):
     with ProtectFile(fname, "r+", backup=False, wait=0.1, max_lock_time=max_lock_time) as pf:
         rewrite(pf, with_copy=with_copy)
     return
 
+
 def change_file_standard(fname, with_copy=False):
     with open(fname, "r+") as pf:  # fails with this context
         rewrite(pf)
     return
+
 
 def init_file(fname):
     with ProtectFile(fname, "w", backup=False, wait=1) as pf:
