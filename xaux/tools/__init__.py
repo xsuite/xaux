5cbb454006cfa4e735f5be1e21bb0eb39bfed213
Frederik Van der Veken <frederik@cern.ch>
2024-07-12 11:44:23 +0200

    Moved some tools from xboinc

---
+++ xaux/tools/__init__.py
@@ -0,0 +1,6 @@
+# copyright ############################### #
+# This file is part of the Xaux Package.    #
+# Copyright (c) CERN, 2024.                 #
+# ######################################### #
+
+from .general_tools import *
