3c94f64faaa4c0917e56549349caf377e5e76130
tpugnat <thomas.pugnat@gmail.com>
2024-07-29 16:30:52 +0200

    tentative fix 'FileNotFoundError' exception from with lockfile.open('x') as flock:.

--- xaux/protectfile.py
+++ xaux/protectfile.py
@@ -232,7 +232,7 @@ class ProtectFile:
                 self._print_debug("init", f"created {self.lockfile}")
                 break
 
-            except FileNotExistsError:
+            except FileNotFoundError:
                 # Lockfile could not be created, wait and try again
                 self._wait(wait)
                 pass
