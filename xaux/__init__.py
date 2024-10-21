09773be9928d1b477cb1a2dc0f5cd605aba8507c
Frederik Van der Veken <frederik@cern.ch>
2024-10-21 16:37:28 +0200

    Crash if python >= 3.12

--- xaux/__init__.py
+++ xaux/__init__.py
@@ -3,6 +3,10 @@
 # Copyright (c) CERN, 2024.                 #
 # ######################################### #
 
+import sys
+if sys.version_info[1] >= 12:
+    raise Exception("Must be using Python 3.8 - 3.11")
+
 from .general import _pkg_root, __version__
 from .protectfile import ProtectFile, ProtectFileError, get_hash
 from .fs import FsPath, LocalPath, EosPath, AfsPath, afs_accessible, eos_accessible, is_egroup_member, cp, mv
