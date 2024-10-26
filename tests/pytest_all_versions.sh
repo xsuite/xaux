5ca284e0ea72b5186f6858dbf12ea2bf2f329a53
Frederik F. Van der Veken <frederik@cern.ch>
2024-10-26 12:58:59 +0200

    Better conda env handling in test

--- tests/pytest_all_versions.sh
+++ tests/pytest_all_versions.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
 
-files="test_fs.py test_afs.py test_eos.py test_fs_api.py"
+files="test_fs.py test_fs_api.py test_afs.py test_eos.py"
 
-# Activate the Mamba base environment
-source /apps/miniforge3/etc/profile.d/conda.sh
-source /apps/miniforge3/etc/profile.d/mamba.sh
-
-mamba activate python3.8
-python -c "import sys; print(f'Testing xaux FS in Python version {sys.version.split()[0]}')"
-pytest $files
-mamba deactivate
-
-mamba activate python3.9
-python -c "import sys; print(f'Testing xaux FS in Python version {sys.version.split()[0]}')"
-pytest $files
-mamba deactivate
-
-mamba activate python3.10
-python -c "import sys; print(f'Testing xaux FS in Python version {sys.version.split()[0]}')"
-pytest $files
-mamba deactivate
-
-mamba activate python3.11
-python -c "import sys; print(f'Testing xaux FS in Python version {sys.version.split()[0]}')"
-pytest $files
-mamba deactivate
-
-#mamba activate python3.12
-#python -c "import sys; print(f'Testing xaux FS in Python version {sys.version.split()[0]}')"
-#pytest $files
-#mamba deactivate
+for i in 8 9 10 11 # 12 13
+do
+    source ~/miniforge3/bin/activate python3.$i
+    python -c "import sys; print(f'Testing xaux FS in Python version {sys.version.split()[0]}')"
+    pytest $files
+    source ~/miniforge3/bin/activate
+done
