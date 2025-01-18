baa18059a83b13c5c1516fb7848a9f90091348a3
Frederik F. Van der Veken <frederik@cern.ch>
2025-01-18 03:20:20 +0100

    Better testing of general tools

--- tests/clean.sh
+++ tests/clean.sh
@@ -6,7 +6,7 @@ do
   fi
 done
 
-for f in example_file.txt
+for f in example_file.txt test_*.json test_*.json.lock test_cronjob.txt
 do
   if [ -e $f ]
   then
@@ -15,6 +15,6 @@ do
 done
 
 rm -r /eos/user/s/sixtadm/test_xboinc/*
-rm -r /afs/cern.ch/user/s/sixtadm/public/test_xboinc
+rm -r /afs/cern.ch/user/s/sixtadm/public/test_xboinc/*
 rm -r level1 level5_res
 
--- tests/test_general_tools.py
+++ tests/test_general_tools.py
@@ -3,16 +3,17 @@
 # Copyright (c) CERN, 2025.                 #
 # ######################################### #
 
+from subprocess import run, TimeoutExpired
 import numpy as np
-import xaux as xa
+from xaux import timestamp, ranID, get_hash, FsPath
 
 
 def test_timestamp():
-    ts = xa.timestamp()
-    ts_f = xa.timestamp(in_filename=True)
-    ts_ms = xa.timestamp(ms=True)
-    ts_ms = xa.timestamp(us=True)
-    ts_ms_f = xa.timestamp(ms=True, in_filename=True)
+    ts = timestamp()
+    ts_f = timestamp(in_filename=True)
+    ts_ms = timestamp(ms=True)
+    ts_us = timestamp(us=True)
+    ts_ms_f = timestamp(ms=True, in_filename=True)
 
     if ts.startswith('21'):
         raise RuntimeError("You really should not be using this code anymore...")
@@ -90,31 +91,67 @@ def test_timestamp():
 
 
 def test_ranID():
-    rans = [xa.ranID(length=l+1) for l in range(40)]
+    rans = [ranID(length=l+1) for l in range(40)]
     print(rans)
     for i, ran in enumerate(rans):
         assert len(ran) == int(np.ceil((i+1)/4)*4)
 
-    rans = [xa.ranID(length=20) for _ in range(1000)]
+    rans = [ranID(length=20) for _ in range(1000)]
     for ran in rans:
         base64_safe = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_'
         assert np.all([c in base64_safe for c in ran])
 
-    rans = [xa.ranID(length=20, only_alphanumeric=True) for _ in range(1000)]
+    rans = [ranID(length=20, only_alphanumeric=True) for _ in range(1000)]
     for ran in rans:
         alnum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
         assert np.all([c in alnum for c in ran])
 
-    rans = xa.ranID(length=20, size=1000)
+    rans = ranID(length=20, size=1000)
     for ran in rans:
         base64_safe = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_'
         assert np.all([c in base64_safe for c in ran])
 
-    rans = xa.ranID(length=20, size=1000, only_alphanumeric=True)
+    rans = ranID(length=20, size=1000, only_alphanumeric=True)
     for ran in rans:
         alnum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
         assert np.all([c in alnum for c in ran])
 
 
 def test_system_lock():
-    pass
+    datafile = FsPath.cwd() / 'test_cronjob.txt'
+    lockfile = FsPath.cwd() / 'test_cronjob.lock'
+    if datafile.exists():
+        datafile.unlink()
+    if lockfile.exists():
+        lockfile.unlink()
+
+    # Normal run
+    cmd1 = run(['python', 'cronjob_example.py'], capture_output=True, text=True)
+    assert cmd1.returncode == 0
+    assert "Cronjob running." in cmd1.stdout
+    assert "Cronjob finished." in cmd1.stdout
+    assert not lockfile.exists()
+    assert not datafile.exists()
+
+    # Run and kill halfway
+    try:
+        cmd2 = run(['python', 'cronjob_example.py'], timeout=2, capture_output=True, text=True)
+    except TimeoutExpired:
+        assert lockfile.exists()
+        assert datafile.exists()
+        datafile.unlink()
+
+    # Run while lockfile exists
+    cmd3 = run(['python', 'cronjob_example.py'], capture_output=True, text=True)
+    assert cmd3.returncode == 1
+    assert "Cronjob running." not in cmd3.stdout
+    assert "Cronjob finished." not in cmd3.stdout
+    assert "Previous test_cronjob.lock script still active!" in cmd3.stderr
+    assert not datafile.exists()
+    lockfile.unlink()
+
+
+def test_hash():
+    hs = get_hash('test_singleton.py')
+    assert hs == '2d4af659d0fdd2779492ffb937e4d04c9eac3bae0c02678297a83077f9548c5001a8b47'\
+               + '71512a02b85628cf5736ebbf7d30071031deb79aa5a391283a093ea4a'
