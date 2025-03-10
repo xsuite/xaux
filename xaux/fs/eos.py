7dacb11f78fc8d7ae96f84ad3782948148abcb1f
Frederik Van der Veken <frederik@cern.ch>
2025-03-10 15:49:02 +0100

    Hotfix: singleton and eos fixes from release/v0.4.0

--- xaux/fs/eos.py
+++ xaux/fs/eos.py
@@ -220,6 +220,9 @@ class EosPath(FsPath, Path):
     def size(self, *args, **kwargs):
         return _eos_size(self.expanduser(), FsPath, *args, **kwargs)
 
+    def as_posix(self, *args, **kwargs):
+        return self.eos_path
+
 
 class EosPosixPath(EosPath, PurePosixPath):
     """EosPath subclass for EOS paths on non-Windows systems.
